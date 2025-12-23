7e51556bddc6cfa7fa5a6d7f8bee6222ddf0b431
jasdox <jason.d.cox07@gmail.com>
2025-12-23 18:00:50 -0500

    finished day 7

---
+++ day7/day7.py
@@ -0,0 +1,45 @@
+import copy
+import sys
+from functools import lru_cache
+
+# Recursively compute the number of ways to reach a position by calling the function on itself at every '^' and stopping when hitting the bottom
+# Additionally uses a memoization cache to speed up already calculated positions
+@lru_cache(None)
+def quantum_count(row, col):
+    global lines
+    if row == len(lines)-1:
+        return 1
+    if lines[row][col] == '^':
+        return quantum_count(row+1, col-1) + quantum_count(row+1, col+1)
+    else:
+        return quantum_count(row+1, col)
+
+
+# Read input and configure for part A
+lines = []
+with open("input.txt", 'r') as f:
+    for line in f.readlines():
+        lines.append(list(line.strip()))
+
+start = -1
+for i in range(len(lines[0])):
+     if lines[0][i] == 'S':
+            lines[0][i] = '|'
+            start = i
+
+count_a = 0
+count_b = quantum_count(1, start)
+
+# Draw light path, splitting at '^' 
+for row in range(1, len(lines)):
+     for col in range(len(lines[0])):
+          if lines[row-1][col] == '|':
+               if lines[row][col] == '^':
+                    count_a += 1
+                    lines[row][col-1] = '|'
+                    lines[row][col+1] = '|'
+               else:
+                    lines[row][col] = '|'
+                
+print("Part A:", count_a)
+print("Part B:", count_b)
